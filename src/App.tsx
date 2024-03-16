15608a7084d6cce6171846bf2365ae60c03e8f58
Luis Riveros <lriveros@internacional.cl>
2024-03-16 00:47:10 -0300

    feat(App): set basic functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import pharLogo from './assets/phar-logo.svg'
 import './App.css'
 
 function App() {
+
+  //valor para el campo de busqueda
+  const [searchCity, setSearchCity] = useState('')
+
+  const handleSearchByLocation = () => {
+    // Get user location
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude)
+    })
+    console.log('search by location')
+  }
+
+  const handleSearchByCity = () => {
+    
+    console.log('search by city ', searchCity)
+  }
 
   return (
     <>
@@ -13,11 +30,20 @@ function App() {
       <h1>Buscar Farmacias</h1>
       
       
-      <Button variant="contained">Buscar Cerca</Button>
+      <Button variant="contained" color="primary" onClick={handleSearchByLocation}>Buscar Cerca</Button>
 
       <h2 className='pinline'><span>tambien puedes</span></h2>
 
-      <TextField id="standard-basic" label="Ciudad" variant="standard" />
+      <TextField id="standard-basic" label="Ciudad" variant="standard" 
+        className='input'
+        value={searchCity}
+        onChange={(e) => setSearchCity(e.target.value)}
+        onKeyUp={(e) => {
+          if (e.key === 'Enter') {
+            handleSearchByCity()
+          }
+        }}
+      />
     </>
   )
 }
